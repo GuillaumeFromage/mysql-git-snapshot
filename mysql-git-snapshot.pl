7f91e12593e46673b84dbf7da97aadc365314ebd
Guillaume <gbeaulieu@koumbit.org>
2016-07-03 15:37:34 -0400

    Added a no-time argument to stop spamming diffs with timestamps

--- mysql-git-snapshot.pl
+++ mysql-git-snapshot.pl
@@ -4,10 +4,11 @@ use warnings;
 use feature 'signatures';
 use feature qw(switch say);
 use Getopt::Long;
+no warnings qw(experimental::signatures);
 
 sub usage() {
     print <<USAGE
-Usage: $0 --db database --debian --branch branch --opt opts --dir directory --message message
+Usage: $0 --db database --debian [--branch branch] [--opt opts] [--dir directory] [--message message] [--ignore-tables tables] [--no-time] [--debug]
 
 This just dumps a database in a git repo in a given directory and commits
 it. 
@@ -16,7 +17,11 @@ it.
 			us this you need to specify user, password and server which
 			aren't implemented yet).
  --branch	which branch to commit to
+ --debug	send more junk to stdout
  --message	the message for the commit
+ --ignore-tables="table1,table2"	a bunch of tables to not drop
+ --no-time	cut the lines that contain the time of the mysqldump, to 
+			reduce to signal ratio
 
 By default, it cuts up the database in individual table files, which can
 just be cat'ed together to reload. Then you can git diff the different
@@ -29,12 +34,12 @@ USAGE
 sub dbdump {
   # whenever dump is called we assumed we're in the right directory and branch
   # which is set by init
-  die "Too many arguments for subroutine" unless @_ <= 4;
   die "Too few arguments for subroutine" unless @_ >= 3;
   $auth = shift(@_);
   $opt = shift(@_);
   $db = shift(@_);
   $split = shift(@_) or 0;
+  @opts = shift(@_) or ();
   `mysqldump $auth $opt --extended-insert=FALSE $db > $db.sql`;
   if ($debug) {
     print "dumped $db !\n\n"; 
@@ -48,7 +53,13 @@ sub dbdump {
     chomp($last);
     # print "csplit -b '%d' -s -f$last $last \"/40103 SET TIME_ZONE=\@OLD_TIME_ZONE/\" {*}";
     `csplit -b '%d' -s -f$last $last "/40103 SET TIME_ZONE=\@OLD_TIME_ZONE/" {*}`;
-    `mv ${last}1 ZZZZfoot.sql`;
+    if ( grep( /^no-time$/, @opts ) ) {
+      `grep -ve'-- Dump completed on' ${last}1 > ZZZZfoot.sql`;
+      `rm ${last}1`;
+      `cat ZZZZfoot.sql`;
+    } else {
+      `mv ${last}1 ZZZZfoot.sql`;
+    }
     @files = ();
     foreach $file (`ls -1 table*`) {
        chomp($file);
@@ -65,7 +76,7 @@ sub dbdump {
 
 }
 
-sub commit_cur_state ($dir, $branch, $auth, $opt, $db, $message) {
+sub commit_cur_state ($dir, $branch, $auth, $opt, $db, $message, @opts) {
   if (! -d $dir) {
     if ($debug) {
       print "initializing git repo of the database dumps through the steps of the upgrade";
@@ -83,7 +94,7 @@ sub commit_cur_state ($dir, $branch, $auth, $opt, $db, $message) {
   } else {
     `git checkout $branch`;
   }
-  commit($message, dbdump($auth, $opt, $db, 1));
+  commit($message, dbdump($auth, $opt, $db, 1, @opts));
 } 
 
 sub commit {
@@ -96,11 +107,10 @@ sub commit {
 $branch = 'master';
 $message = 'mysql git snapshot autocommit ' . time();
 $opt = $db = $directory = '';
-$debian = 0;
-
-GetOptions ('db=s' => \$db, 'branch=s' => \$branch, 'opt=s' => \$opt, 'debian' => \$debian, 'dir=s' => \$directory, 'message=s' => \$message)
+$debug = $notime = $debian = 0;
+@opts = ();
+GetOptions ('db=s' => \$db, 'branch=s' => \$branch, 'opt=s' => \$opt, 'debian' => \$debian, 'dir=s' => \$directory, 'message=s' => \$message,'no-time' => \$notime, 'debug' => \$debug)
   or usage();
-
 
 if ($db eq '') {
    usage();
@@ -111,6 +121,10 @@ if ($directory eq '') {
    $directory = $db;
 }
 
+if ($notime) {
+  push(@opts, 'no-time');
+} 
+
 if (!$debian ) {
   #TODO or if there is no other way of authing against the database server
   usage();
@@ -119,4 +133,4 @@ if (!$debian ) {
   $auth = " --defaults-file=/etc/mysql/debian.cnf";
 }
 
-commit_cur_state($directory, $branch, $auth, $opt, $db, $message);
+commit_cur_state($directory, $branch, $auth, $opt, $db, $message, @opts);
